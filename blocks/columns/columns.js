11f6f52e3c5fd0113922e3182de0d0ea1c0a494f
santosh.m <iamsr09@gmail.com>
2023-06-08 17:07:45 +0530

    css changes

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -16,3 +16,22 @@ export default function decorate(block) {
     });
   });
 }
+
+export default function decorate(blockapp) {
+  const cols = [...blockapp.firstElementChild.children];
+  blockapp.classList.add(`columnsdesignapp-${cols.length}-cols-columnsdesignapp`);
+
+  // setup image columns
+  // [...block.children].forEach((row) => {
+  //   [...row.children].forEach((col) => {
+  //     const pic = col.querySelector('picture');
+  //     if (pic) {
+  //       const picWrapper = pic.closest('div');
+  //       if (picWrapper && picWrapper.children.length === 1) {
+  //         // picture is only content in column
+  //         picWrapper.classList.add('columnsdesignapp-img-col');
+  //       }
+  //     }
+  //   });
+  // });
+}
